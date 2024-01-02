6c8275b002ff74357321ae0160921626fb980895
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-02 22:30:01 +0200

    Create Next app

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='bg-black h-screen'>
+      <h2 className='text-4xl text-green-400'>Dashboard</h2>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-between p-24">
+     <h2>This is my song my Savior gave to me</h2>
+    </main>
+  )
+}
